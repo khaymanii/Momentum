52f6406fb8881e4c0c6a9310d7c225e205aefc8f
unknown <kennethakpo61@gmail.com>
2026-08-01 05:20:10 +0100

    reviewing codebase

--- components/auth/SignUpForm.tsx
+++ components/auth/SignUpForm.tsx
@@ -8,7 +8,7 @@ import { PasswordInput } from "./PasswordInput";
 export function SignUpForm() {
   const [isLoading, setIsLoading] = useState(false);
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
 
     setIsLoading(true);
@@ -100,5 +100,3 @@ export function SignUpForm() {
     </>
   );
 }
-
-
