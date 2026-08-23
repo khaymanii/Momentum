e814fd038aadbf4ee2a5c42176f59fbbb5d97ea6
unknown <kennethakpo61@gmail.com>
2026-08-23 23:26:44 +0100

    refactor: improve code formatting and readability in ResetPasswordPage and firebase-client

--- lib/firebase-client.ts
+++ lib/firebase-client.ts
@@ -3,5 +3,13 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const config = { apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID };
-export const firebaseAuth = getAuth(getApps().length ? getApp() : initializeApp(config));
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export const firebaseAuth = getAuth(
+  getApps().length ? getApp() : initializeApp(config),
+);
